3c37f51e592bd218e65ec88b3c6665cd61a2e796
woon7 <erty950@naver.com>
2025-07-29 22:45:20 +0900

    Update : model, dto 추가

--- src/main/java/com/example/login/movie/dto/MovieDto.java
+++ src/main/java/com/example/login/movie/dto/MovieDto.java
@@ -1,4 +1,31 @@
 package com.example.login.movie.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public class MovieDto {
+
+    //영화코드
+    private String movieCd;
+    //영화명
+    private String movieNm;
+    //영화명
+    private String moviceNmEn;
+    //제작국가
+    private String repNationAlt;
+    //장르
+    private String repGenreAlt;
+    //영화감독
+    private String directors;
+    //제작사코드
+    private String companyCd;
+    //제작사명
+    private String companyNm;
+
+    //제작연도
+    private String prdtYear;
+    //개봉일
+    private String openDt;
 }
--- src/main/java/com/example/login/movie/model/Movie.java
+++ src/main/java/com/example/login/movie/model/Movie.java
@@ -1,4 +1,42 @@
 package com.example.login.movie.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public class Movie {
+
+    @Id
+    @Column(name = "MOVIE_CD", unique = true, nullable = false)
+    private String movieCd;
+
+    @Column(name = "MOVIE_NM")
+    private String movieNm;
+
+    @Column(name = "MOVIE_NM_EN")
+    private String moviceNmEn;
+
+    @Column(name = "REP_NATION_ALT")
+    private String repNationAlt;
+
+    @Column(name = "REP_GENRE_ALT")
+    private String repGenreAlt;
+
+    @Column(name = "DIRECTORS")
+    private String directors;
+
+    @Column(name = "COMPANY_CD")
+    private String companyCd;
+
+    @Column(name = "COMPANY_NM")
+    private String companyNm;
+
+    @Column(name = "PRDT_YEAR")
+    private String prdtYear;
+
+    @Column(name = "OPEN_DT")
+    private String openDt;
 }
