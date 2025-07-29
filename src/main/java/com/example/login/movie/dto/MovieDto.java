8d0bc16bf01be174e27fb65ce0b398a866521c3b
woon7 <erty950@naver.com>
2025-07-29 22:45:27 +0900

    no message

--- src/main/java/com/example/login/movie/dto/MovieDto.java
+++ src/main/java/com/example/login/movie/dto/MovieDto.java
@@ -1,9 +1,11 @@
 package com.example.login.movie.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
+@Builder
 @AllArgsConstructor
 public class MovieDto {
 
